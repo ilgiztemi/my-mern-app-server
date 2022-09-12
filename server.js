29e885065633e903107531f30fdf011871a425c1
Ilgiz Temi <ilgiztemi@Ilgizs-MacBook-Pro.local>
2022-09-12 10:55:41 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require( "express" );
+const app = express();
+const bodyParser = require( "body-parser" );
+const cors = require( "cors" );
+const mongoose = require( "mongoose" );
+const path = require( "path" );
+require( "dotenv" ).config();
+const { MONGO_URI } = process.env;
+const port = process.env.PORT || 4002;
+
+app.use( bodyParser.json() );
+app.use( cors() );
+
+//mongoose
+mongoose.connect( MONGO_URI )
+
+
+//path 
+
+// app.use( express.static( path.join( __dirname, "./client/build" ) ) );
+// app.get( "*", function ( req, res ) {
+//     res.sendFile( path.join( __dirname + "/./client/build/index.html" ) );
+// } );
+
+//data schema and model
+const movieSchema = {
+    title: String,
+    genre: String,
+    year: String
+}
+
+const Movie = mongoose.model( "Movie", movieSchema );
+
+//api routes
+app.get( '/movies', ( req, res ) => {
+    Movie.find().then( movies => res.json( movies ) )
+} )
+app.post( '/newMovie', ( req, res ) => {
+    const title = req.body.title;
+    const genre = req.body.genre;
+    const year = req.body.year;
+    const newMovie = new Movie( {
+        title,
+        genre,
+        year
+    } )
+    newMovie.save();
+} )
+app.delete( '/delete/:id', ( req, res ) => {
+    Movie.findByIdAndDelete( { _id: req.params.id }, ( err, data ) => {
+        if ( err ) {
+            console.log( err );
+        }
+        else {
+            console.log( "movie deleted", data )
+        }
+    } )
+} )
+
+app.listen( port, () => {
+    console.log( "express is running on " + ` ${ port }` )
+} )
